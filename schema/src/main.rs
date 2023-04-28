0ddb42e0c3f6727e5f6a0cd8f05e2b4219658f1e
marcasmar1 <castellmartinmarco1@gmail.com>
2023-04-28 14:20:29 +0200

    partial commit

--- schema/src/main.rs
+++ schema/src/main.rs
@@ -61,7 +61,7 @@ pub struct Record {
     /// https://datatracker.ietf.org/doc/html/rfc3339#section-5.6)
     #[schemars(regex(pattern = r"^((?:(\d{4}-\d{2}-\d{2})T(\d{2}:\d{2}:\d{2}(?:\.\d+)?))Z?)$"))]
     time: String,
-    /// An unique number that identifies this record between all other ones present in the document.
+    /// A unique number that identifies this record between all other ones present in the document.
     record_number: u64,
     /// Classification of the record.
     classification: Classification,
@@ -78,8 +78,8 @@ pub struct Record {
     /// Free form text, possibly describing the model or configuration of the UAS identified.
     identification: String,
     /// The 3D GPS location of the CUAS recorded on this instant. Overrides the document's
-    /// static_cuas_location.
-    cuas_location: Position3d,
+    /// static_cuas_location if included.
+    cuas_location: Option<Position3d>,
 }
 
 #[derive(JsonSchema)]
@@ -93,24 +93,37 @@ pub enum Classification {
 #[derive(JsonSchema)]
 pub enum Location {
     // clockwise: from -> to (degrees)
+    /// Circular arc within which the UAS has been observed.
     Arc(Arc),
+    /// Compass quadrant where the UAS has been observed
     Quad(Quad),
     // clockwise from true north (degrees)
+    /// Clockwise angle in degrees from true north where the UAS has been observed.
     Bearing(f64),
+    /// Flat 2D position of the UAS given in Latitude and Longitude
     Position2d(Position2d),
+    /// 3D position of the UAS given in Latitude, Longitude and height.
     Position3d(Position3d),
+    /// Ray where the UAS has been observed, given in bearing and elevation
     BearingElevation {
+        /// Clockwise angle in degrees from true north where the UAS has been observed.
         bearing: f64,
+        /// Elevation over the horizon of the UAS relative to the CUAS given in degrees.
         elevation: f64,
     },
+    /// 3D position of the UAS given in bearing, elevation angle and distance.
     BearingElevationDistance {
+        /// Clockwise angle in degrees from true north where the UAS has been observed.
         bearing: f64,
+        /// Elevation over the horizon of the UAS relative to the CUAS given in degrees.
         elevation: f64,
+        /// Distance from the UAS to the CUAS given in metres.
         distance: f64,
     },
 }
 
 #[derive(JsonSchema)]
+/// Circular arc within which the UAS has been observed.   
 pub struct Arc {
     /// Minimum compass angle from the CUAS System to the UAS in degrees.
     from: f64,
@@ -119,6 +132,16 @@ pub struct Arc {
 }
 
 #[derive(JsonSchema)]
+/// Compass quadrant where the UAS has been observed
+pub enum Quad {
+    North,
+    East,
+    South,
+    West,
+}
+
+#[derive(JsonSchema)]
+/// Flat 2D position of the UAS given in Latitude and Longitude
 pub struct Position2d {
     /// GPS latitude of the UAS measured in degrees.
     lat: f64,
@@ -127,21 +150,14 @@ pub struct Position2d {
 }
 
 #[derive(JsonSchema)]
+/// 3D position of the UAS given in Latitude, Longitude and height.
 pub struct Position3d {
     /// GPS latitude of the UAS measured in degrees.
     lat: f64,
     /// GPS longitude of the UAS measured in degrees.
     lon: f64,
-    /// Height of the UAS measured in meters from sea level.
+    /// Height of the UAS measured in metres from sea level.
     height: f64,
-}
-
-#[derive(JsonSchema)]
-pub enum Quad {
-    North,
-    East,
-    South,
-    West,
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
